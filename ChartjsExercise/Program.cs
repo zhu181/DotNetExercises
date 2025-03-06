fe783330de6d46282d69f57a593e2b2eb5d36698
zhu181 <36814419+zhu181@users.noreply.github.com>
2025-03-06 14:52:12 +0800

    更新项目至 .NET 9.0 并升级依赖包
    所有项目的目标框架已从 `net8.0` 更新为 `net9.0`。多个依赖包版本进行了更新，包括 `AutoMapper`、`BouncyCastle.Cryptography`、`Microsoft.AspNetCore.Components.WebAssembly` 等，以确保项目使用最新的功能和修复。
    
    此外，在 `BarSimple.razor`、`LineSimple.razor` 和 `PieSimple.razor` 文件中，背景色和边框色的引用已更新为 `ChartjsExercise.Model.Colors`。在 `Program.cs` 中，异步代码的调用方式也进行了优化，提升了性能。

--- ChartjsExercise/Program.cs
+++ ChartjsExercise/Program.cs
@@ -13,7 +13,7 @@ namespace ChartjsExercise
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
+            await builder.Build().RunAsync().ConfigureAwait(false);
         }
     }
 }
